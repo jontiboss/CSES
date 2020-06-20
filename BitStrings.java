b124a1ee7921784163018df5c410ecdd0fe278b2
MediumBrain <kappalaie@hotmail.com>
2020-06-20 09:15:23 +0200

    Add files via upload

---
+++ BitStrings.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class BitStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Reader reader = new Reader();
+		BigInteger bits = BigInteger.valueOf(reader.nextInt());
+		BigInteger ans = BigInteger.TWO;
+		BigInteger mod = BigInteger.valueOf(1000000007);
+		System.out.println(ans.modPow(bits, mod).toString());
+	}
+	static class Reader 
+	{ 
+		BufferedReader br; 
+		StringTokenizer st; 
+
+		public Reader() 
+		{ 
+			br = new BufferedReader(new
+					InputStreamReader(System.in)); 
+		} 
+
+		String next() 
+		{ 
+			while (st == null || !st.hasMoreElements()) 
+			{ 
+				try
+				{ 
+					st = new StringTokenizer(br.readLine()); 
+				} 
+				catch (IOException e) 
+				{ 
+					e.printStackTrace(); 
+				} 
+			} 
+			return st.nextToken(); 
+		} 
+
+		int nextInt() 
+		{ 
+			return Integer.parseInt(next()); 
+		} 
+	}
+}
